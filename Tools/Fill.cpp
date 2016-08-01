c19aad14132a084bab2fcf0a5da791379f474feb
RedYurii <vaultboy@ukr.net>
2016-08-01 19:19:29 +0300

    Create Fill.cpp

---
+++ Tools/Fill.cpp
@@ -0,0 +1,17 @@
+// Fill.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "Fill.h"
+
+void Fill::Work(MyCOORD crd, HDC hDC, int thick, COLORREF color) //implementation of virtual function work to allow filling closed areas with colored brush
+{
+	HBRUSH hBr = CreateSolidBrush(color);
+	HBRUSH hOldBr = (HBRUSH)SelectObject(hDC, hBr);
+	COLORREF bgColor = GetPixel(hDC, crd.mxInit, crd.myInit);
+	ExtFloodFill(hDC, crd.mxInit, crd.myInit, bgColor, FLOODFILLSURFACE);
+	SelectObject(hDC, hOldBr);
+	DeleteObject(hBr);
+}
+
+
